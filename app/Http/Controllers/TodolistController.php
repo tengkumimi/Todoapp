00472b290233fa7dd52b1d1b80861f197116978a
Tengku <tengkumimiwahidah@gmail.com>
2023-01-25 22:47:42 +0800

    first commit

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todolists = Todolist::all();
+        return view('home', compact('todolists'));
+    }
+    
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'content' => 'required'
+        ]);
+
+        Todolist::create($data);
+        return back();
+    }
+    
+    public function destroy(Todolist $todolist)
+    {
+        $todolist->delete();
+        return back();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\TodolistController@index')->name('index');
+Route::post('/', 'App\Http\Controllers\TodolistController@store')->name('store');
+Route::delete('/{todolist:id}', 'App\Http\Controllers\TodolistController@destroy')->name('destroy');
